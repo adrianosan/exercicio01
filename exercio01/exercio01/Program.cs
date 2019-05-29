1bb4e5ffa2f6a1b63d0f8997d704517aa2dd2846
adriano.santos <adriano.santos@squadra.com.br>
2019-05-29 09:01:30 -0300

    EXERCICIO01

---
+++ exercio01/exercio01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("É Preciso FAZER TODOS OS ALGORITMOS PARA APRENDER");
+        }
+    }
+}
